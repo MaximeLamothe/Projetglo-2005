2a8db40ffbfbba9c08d8ea2520b3db0e4ea1ceef
Julianne Fréchette <julianne.frechette.1@ulaval.ca>
2025-03-16 22:25:49 -0400

    ajout pages html login et inscription

--- server.py
+++ server.py
@@ -11,7 +11,15 @@ database = Database()
 def main():
     return render_template('login.html')
 
+@app.route("/login")
+def login():
+    # if
+        # return render_template('bienvenu.html', profile=ProfileUtilisateur)
+    return render_template('login.html')
 
+@app.route("/inscription/")
+def inscription():
+    return render_template('inscription.html')
 
 if __name__ == "__main__":
     app.run()
