b8c827fe2b8cee9f2064e6bc667f6a9296a4bb20
MaximeLamothe <maxime.lamothe.5@ulaval.ca>
2025-03-21 02:14:48 -0400

    database

---
+++ Dump20250321/projet_livres.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projet
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `livres`
+--
+
+DROP TABLE IF EXISTS `livres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `livres` (
+  `lid` int NOT NULL,
+  `titre` varchar(50) NOT NULL,
+  `genre` varchar(50) NOT NULL,
+  `annee` int DEFAULT NULL,
+  `maison_edition` varchar(50) DEFAULT NULL,
+  `couverture` varchar(50) DEFAULT NULL,
+  `nombre_de_pages` int DEFAULT NULL,
+  `note` decimal(3,2) DEFAULT NULL,
+  PRIMARY KEY (`lid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `livres`
+--
+
+LOCK TABLES `livres` WRITE;
+/*!40000 ALTER TABLE `livres` DISABLE KEYS */;
+INSERT INTO `livres` VALUES (1,'Les Misérables','Roman',1862,'Gallimard','les_miserables.jpg',1232,NULL),(2,'Le Tour du monde en 80 jours','Aventure',1873,'Hachette','tour_du_monde.jpg',320,NULL),(3,'1984','Dystopie',1949,'Secker & Warburg','1984.jpg',328,NULL),(4,'Pride and Prejudice','Romance',1813,'T. Egerton','pride_prejudice.jpg',432,NULL),(5,'Notre-Dame de Paris','Roman',1831,'Librairie Gosselin','notre_dame.jpg',940,NULL),(6,'Vingt Mille Lieues sous les mers','Science-fiction',1870,'Pierre-Jules Hetzel','20m_lieues.jpg',672,NULL);
+/*!40000 ALTER TABLE `livres` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-21  2:13:07
